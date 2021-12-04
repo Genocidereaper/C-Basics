54373fd24072d8d0a8f86b46d3451a194f407dab
MrPiddlesticks <44594756+MrPiddlesticks@users.noreply.github.com>
2021-12-04 15:33:51 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,29 +1,11 @@
 #include <iostream>
 #include "Object.h"
+#include "Actor.h"
+#include "Pawn.h"
+
 using namespace std;
 
-
-class Actor : public Object
-{
-public:
-	virtual void BeginPlay() override;
-	void ActorFunction()
-	{
-		cout << "ActorFunction() called.\n\n";
-	}
-};
-class Pawn : public Actor
-{
-public:
-	virtual void BeginPlay() override;
-	void PawnFunction()
-	{
-		cout << "PawnFunction() called.\n\n";
-	}
-};
-
-int main()
-{
+void InhertianceFunction() {
 	Object* ptr_to_object = new Object;
 	Actor* ptr_to_actor = new Actor;
 	Pawn* ptr_to_pawn = new Pawn;
@@ -33,11 +15,11 @@ int main()
 
 	for (int i = 0; i < 3; i++)
 	{
-		//ObjectArray[i]->BeginPlay();
+
 		Object* obj = ObjectArray[i];
 
 		Actor* act = dynamic_cast<Actor*>(obj);
-		// static cast will be successful every time unlike dynamic cast there is no runtime cast checking
+
 		if (act)
 		{
 			act->ActorFunction();
@@ -54,14 +36,10 @@ int main()
 	delete ptr_to_object;
 	delete ptr_to_actor;
 	delete ptr_to_pawn;
-	system("pause");
 }
 
-void Actor::BeginPlay()
+int main()
 {
-	cout << "Actor BeginPlay() called. \n\n";
-}
-void Pawn::BeginPlay()
-{
-	cout << "Pawn BeginPlay() called. \n\n";
+	InhertianceFunction();
+	system("pause");
 }
